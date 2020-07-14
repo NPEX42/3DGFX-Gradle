87c8893cab255037e910f2d3ee5976326821e777
Npex <gvenn666@gmail.com>
2020-07-14 12:24:03 +0100

    Updated sandbox to use new systems

--- 3DGFX/src/main/java/uk/co/nerdprogramming/gfx/engine/Geometry.java
+++ 3DGFX/src/main/java/uk/co/nerdprogramming/gfx/engine/Geometry.java
@@ -24,9 +24,39 @@ public class Geometry {
 		this.mesh = mesh;
 	}
 	
+	public static Geometry LoadGeom(String meshPath, String shaderName, String vertexPath, String fragPath, String albedoFilePath, Color tintColor) {
+		ShaderManager.LoadShader(shaderName, vertexPath, fragPath);
+		Texture2D albedo;
+		if(albedoFilePath == null) {
+			albedo = Texture2D.white;
+		} else {
+			albedo = Texture2D.Load(albedoFilePath);
+		}
+		Material mat = new Material(shaderName, tintColor, albedo);
+		mat.SetupAttributeLayout("a_Positions","a_UVS");
+		Mesh mesh = MeshLoader.LoadMesh(meshPath);
+		
+		return new Geometry(mat, mesh);
+	}
+	
+	public static Geometry LoadGeom(String meshPath, String shaderName, String albedoFilePath, Color tintColor) {
+		Texture2D albedo;
+		if(albedoFilePath == null) {
+			albedo = Texture2D.white;
+		} else {
+			albedo = Texture2D.Load(albedoFilePath);
+		}
+		Material mat = new Material(shaderName, tintColor, albedo);
+		mat.SetupAttributeLayout("a_Positions","a_UVS");
+		Mesh mesh = MeshLoader.LoadMesh(meshPath);
+		
+		return new Geometry(mat, mesh);
+	}
+	
 	static {
+		ShaderManager.LoadShader("3d/basic", "uk/co/nerdprogramming/gfx/res/shaders/3d.vs.glsl", "uk/co/nerdprogramming/gfx/res/shaders/3d.fs.glsl");
 		error = new Geometry(
 				new Material("3d/basic", Color.white, Texture2D.missing), 
-				MeshLoader.LoadMesh("NONEXISTEN"));
+				MeshLoader.LoadMesh("NONEXISTENT"));
 	}
 }
--- Sandbox/src/Main.java
+++ Sandbox/src/Main.java
@@ -15,58 +15,16 @@ public class Main {
 		System.err.println("[Sandbox] Opening Window...");
 		DisplayManager.Open(1080, 720, "Sandbox Application");
 		GLRenderer glr = GLRenderer.get();
-		
-//		float[] pos = {
-//				-1, -1, 0,
-//				+1, -1, 0,
-//				+1, +1, 0,
-//				-1, +1, 0,
-//		};
-//		
-//		float[] uvs = {
-//				0,0,
-//				1,0,
-//				1,1,
-//				0,1,
-//		};
-//		
-//		int[] tris = {0,1,2,2,3,0};
-		System.err.println("[Sandbox] Building Mesh...");
-		//MeshLoader.ConvertOBJ("res/cube.obj");
-		Mesh quad = MeshLoader.LoadMesh("res/torus.mesh");
-		
-		ShaderManager.LoadShader("3d/basic",
-				"uk/co/nerdprogramming/gfx/res/shaders/3d.vs.glsl",
-				"uk/co/nerdprogramming/gfx/res/shaders/3d.fs.glsl"
+		Geometry quad = Geometry.LoadGeom(
+				"res/quad.mesh",
+				"3d/basic",
+				null,
+				Color.blue
 		);
-		
-		Material mat = new Material("3d/basic", Color.white, Texture2D.white);
-		
-		Geometry geom = new Geometry(mat, quad);
-		
-		long tp1, tp2, frameCount = 0, DC = 0, VC = 0;
-		double tpfs, accumTime = 0;
-		
 		while(DisplayManager.Update()) {
-			tp1 = System.nanoTime();
-			glr.ClearColor(Color.BLUE);
-			mat.PrepareShader();
-			for(int i = 0; i < 1; i++) {
-				glr.Render(Geometry.GetErrorGeom());
-				DC++; //Increment the number of draw calls this frame.
-				VC += quad.GetVertexCount(); 
-			}
-			tp2 = System.nanoTime();
-			tpfs = (tp2 - tp1) / 1000000000f;
-			System.err.printf("TPF: %03.9fS (%03.4fms) | FPS: %.2f | DC: %d | Vertex Count: %d                          \r",tpfs, tpfs * 1000f, 1f / tpfs, DC, VC);
-			accumTime += tpfs;
-			frameCount++;
-			DC = 0;
-			VC = 0;
+			glr.ClearColor(Color.GRAY);
+			glr.Render(quad);
 		}
-		
-		double avg = accumTime / frameCount;
-		System.err.printf("\nAverage Frame Time: %.4fms (%.2f fps)",avg * 1000f,1f/avg);
 		DisplayManager.Close();
 	}
 
